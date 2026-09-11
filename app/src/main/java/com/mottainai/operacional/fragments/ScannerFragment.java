ff6e7c0fd4a75b26d65d91c6decbb02ba936c257
AnaClara66sy <Ana1setembro09@gamil.com>
2026-09-11 06:43:34 -0300

    fix: resolve compilation errors in ProductRepository, MockProductRepository and ScannerFragment

--- app/src/main/java/com/mottainai/operacional/fragments/ScannerFragment.java
+++ app/src/main/java/com/mottainai/operacional/fragments/ScannerFragment.java
@@ -9,6 +9,7 @@ import android.hardware.camera2.CameraCharacteristics;
 import android.media.ToneGenerator;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Looper;
 import android.os.Vibrator;
 import android.provider.Settings;
 import android.util.Log;
